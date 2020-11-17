3ffe5ef441ce31984a9a9c29b11eb0297e1f4c52
koheihosono <koheihosono3141@gmail.com>
2020-11-17 11:14:04 +0900

    komittosimasita

--- Shiken8/Shiken8/Shiken8.cpp
+++ Shiken8/Shiken8/Shiken8.cpp
@@ -1,18 +1,18 @@
 ﻿#include <iostream>
 using namespace std;
 
-void sumEx(int& a,int& b)
+void sumEx(int& c)
 {
-	int c = a + b;
+	int a = 1;
+	int b = 2;
+	c = a + b;
 }
 
 int main()
 {
 	int plus;
-	int plus2;
 
 	cin >> plus;
-	cin >> plus2;
 
-	sumEx(plus,plus2);
+	sumEx(plus);
 }
